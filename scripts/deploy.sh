5a303a36e17b69bdda7f1403604d62c216522627
Christoph Oelmüller <christoph.oelmueller@epost-dev.de>
2017-04-02 23:03:46 +0200

    prefix ssh key with TRAVIS_BUILD_DIR

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ TARGETUSER=sub11429_64
 
 error_n_exit() {
     msg=$1
-    rm -f id_rsa
+    rm -f $TRAVIS_BUILD_DIR/id_rsa
     echo $msg
     exit 1
 }
@@ -15,12 +15,12 @@ error_n_exit() {
 [ -d "$TRAVIS_BUILD_DIR/public" ] && tar cvzf $TARBALL -C $TRAVIS_BUILD_DIR/public .
 
 # upload
-scp -o StrictHostKeyChecking=no -i id_rsa $TARBALL $TARGETUSER@$TARGETHOST:
+scp -o StrictHostKeyChecking=no -i $TRAVIS_BUILD_DIR/id_rsa $TARBALL $TARGETUSER@$TARGETHOST:
 if [ $? -ne 0 ];then
     error_n_exit "failed to upload tarball to $TARGETHOST..."
 fi
 
-rm -f id_rsa
+rm -f $TRAVIS_BUILD_DIR/id_rsa
 
 
 
