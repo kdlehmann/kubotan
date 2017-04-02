b1bfe15e534acf674c584977909b845a1174aebd
Christoph Oelmüller <christoph.oelmueller@epost-dev.de>
2017-04-02 22:42:47 +0200

    switch to script deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+TARBALL=kubotan-$(date +%s).tar.gz
+TARGETHOST=46.38.249.61
+TARGETUSER=sub11429_64
+
+function error_n_exit() {
+    msg=$1
+    rm -f id_rsa
+    echo $msg
+    exit 1
+}
+
+# tar build
+[-d "$TRAVIS_BUILD_DIR/public" ] && tar cvzf $TARBALL -C $TRAVIS_BUILD_DIR/public .
+
+# upload
+scp -i id_rsa $TARBALL $TARGETUSER@$TARGETHOST:
+if [ $? -ne 0 ];then
+    error_n_exit "failed to upload tarball to $TARGETHOST..."
+fi
+
+rm -f id_rsa
+
+
+
